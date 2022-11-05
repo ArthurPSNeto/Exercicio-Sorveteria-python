820a0032d63b540f58f1aac6f0769163765b16c7
Arthur Souza <111993289+ArthurPSNeto@users.noreply.github.com>
2022-11-05 18:54:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# complexidade ciclomática
+
+preco=0
+op=0        
+print("Ola, somos uma sorveteria. Nos temos 2 tipos de sorvete: Premium e Comum. O premium custa R$20,00 e o comum custa R$ 15,00\n1 - Sorvete premium\n2 - Sorvete comum\n")
+preco=int(input("\nQual tipo de sorvete você vai querer?\n"))
+if preco==1:
+    preco=20
+else:
+    preco=15
+
+op=int(input("\nNos vendemos o sorvete em copinho ou em casquinha. O valor da casquinha e R$2,00 e o valor do copinho e R$1,00. Qual recipiente você vai escolher?\n1 - Copinho:\n2 - Casquinha:\n"))
+if op==1:
+    
+    preco=preco+1
+else:
+    preco=preco+2
+
+op=int(input("\nA cobertura pode ser apenas uma por R$1 ou cobertura especial(mais de uma) por apenas R$2,00. Qual você vai querer?\n1 - Simples\n2 - Especial\n"))
+if op==1:
+    preco=preco+1
+else:
+    preco=preco+2
+
+print("\nVocê vai pagar: ",preco)
